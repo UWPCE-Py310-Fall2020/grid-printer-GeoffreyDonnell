db98c634f271a30390090006dbe235d82214c8a3
Geoffrey Donnell <geoffwd@uw.edu>
2021-04-13 09:54:36 -0700

    I've completed part 1 of the grid printer excersise

--- grid_printer.py
+++ grid_printer.py
@@ -4,11 +4,25 @@ Update below functions with code to print different grids based on the input par
 NOTE: do not print anything besides the grid boxes in your functions.
 """
 
-
+plus = '+'
+dash = '-'
+space = ' '
+line = '|'
 # PART 1
 def print_grid1():
-    pass
-
+    n = 4
+    print(plus + n*(space + dash) + space + plus + n*(space + dash) + space + plus)
+    print(line + n * (2*space) + space + line + n * (2*space) + space + line)
+    print(line + n * (2*space) + space + line + n * (2*space) + space + line)
+    print(line + n * (2*space) + space + line + n * (2*space) + space + line)
+    print(line + n * (2*space) + space + line + n * (2*space) + space + line)
+    print(plus + n * (space + dash) + space + plus + n * (space + dash) + space + plus)
+    print(line + n * (2*space) + space + line + n * (2*space) + space + line)
+    print(line + n * (2*space) + space + line + n * (2*space) + space + line)
+    print(line + n * (2*space) + space + line + n * (2*space) + space + line)
+    print(line + n * (2*space) + space + line + n * (2*space) + space + line)
+    print(plus + n * (space + dash) + space + plus + n * (space + dash) + space + plus)
+#end of function blocok
 
 # PART 2
 def print_grid2(size):
@@ -18,3 +32,5 @@ def print_grid2(size):
 # PART 3
 def print_grid3(box_size, cell_size):
     pass
+
+print_grid1()
